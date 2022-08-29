dc2aaff191fd7110e00b0756f7c704e65b48f337
Soheeb Amin <soheeb@gmail.com>
2022-08-29 10:59:58 -0500

    table generated

--- src/index.js
+++ src/index.js
@@ -3,14 +3,11 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import reportWebVitals from "./reportWebVitals";
 import "bootstrap/dist/css/bootstrap.css";
-import MoviesTable from "./components/MoviesTable";
+import App from "./App";
+import Movies from "./components/movies";
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
-  <React.StrictMode>
-    <MoviesTable />
-  </React.StrictMode>
-);
+root.render(<App />);
 
 // If you want to start measuring performance in your app, pass a function
 // to log results (for example: reportWebVitals(console.log))
