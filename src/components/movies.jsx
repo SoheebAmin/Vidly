d7ab12d60c1e81a1bd89561a8336394ade83de77
Soheeb Amin <soheeb@gmail.com>
2022-08-30 08:06:43 -0500

    Vidly complete

--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -6,29 +6,51 @@ class Movies extends Component {
     movies: getMovies(),
   };
 
+  // arrow function to bind this to the object
+  handleDelete = (movie) => {
+    const movies = this.state.movies.filter((m) => m._id !== movie._id); // filtering movies to return all movies without the id of parameter movie.
+    this.setState({ movies: movies }); // re-recreating the above state object by setting "movies" to the just-created const.
+    // Note: In JS, if key and value are the same like here, we can just say "movies" once:
+    // this.setsState {{ movies }}
+  };
+
   render() {
-    return (
-      <table className="table">
-        <thead>
-          <tr>
-            <th>Title</th>
-            <th>Genre</th>
-            <th>Stock</th>
-            <th>Rate</th>
-          </tr>
-        </thead>
-        <tbody>
-          {this.state.movies.map((movie) => (
+    const { length: count } = this.state.movies; // a bit of an odd way to just say" count = this.state.movies.length
+    if (count === 0) {
+      return <p>No Movies Here!</p>;
+    } else
+      return (
+        <table className="table">
+          <thead>
             <tr>
-              <td>{movie.title}</td>
-              <td>{movie.genre.name}</td>
-              <td>{movie.numberInStock}</td>
-              <td>{movie.dailyRentalRate}</td>
+              <th>Title</th>
+              <th>Genre</th>
+              <th>Stock</th>
+              <th>Rate</th>
+              <th></th>
             </tr>
-          ))}
-        </tbody>
-      </table>
-    );
+          </thead>
+          <tbody>
+            {/** On line 29, key is added to generate unique id for each element */}
+            {this.state.movies.map((movie) => (
+              <tr key={movie._id}>
+                <td>{movie.title}</td>
+                <td>{movie.genre.name}</td>
+                <td>{movie.numberInStock}</td>
+                <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <button
+                    onClick={() => this.handleDelete(movie)} // arrow function to pass argument
+                    className="btn btn-danger btn-sm"
+                  >
+                    Delete
+                  </button>
+                </td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      );
   }
 }
 
