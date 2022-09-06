37880eedc87027bec54c554a28ba30c758f3d20a
Soheeb <soheeb@gmail.com>
2022-09-06 11:59:55 -0500

    end of composing components section

---
+++ src/components/common/like.jsx
@@ -0,0 +1,25 @@
+import React, { Component } from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+  faHeartBroken,
+  faHeartCircleCheck,
+} from "@fortawesome/free-solid-svg-icons";
+
+// Input: Needs to know if given move is liked or not.
+// Output: The events raised. It should raise an onClock and toggle like property.
+
+const Like = (props) => {
+  var heart = faHeartBroken;
+
+  if (props.liked) heart = faHeartCircleCheck;
+
+  return (
+    <FontAwesomeIcon
+      icon={heart}
+      style={{ cursor: "pointer" }}
+      onClick={props.onClick}
+    />
+  );
+};
+
+export default Like;
--- src/components/movies.jsx
+++ src/components/movies.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from "react";
-import { getMovies } from "../services/fakeMovieService";
+import Like from "./common/like";
+import { getMovies, movies } from "../services/fakeMovieService";
 
 class Movies extends Component {
   state = {
@@ -12,6 +13,15 @@ class Movies extends Component {
     this.setState({ movies: movies }); // re-recreating the above state object by setting "movies" to the just-created const.
     // Note: In JS, if key and value are the same like here, we can just say "movies" once:
     // this.setsState {{ movies }}
+  };
+
+  // note all of these changes only persistent in UI. We will also need to make an HTTP request to save changes in DB on server
+  handleLike = (movie) => {
+    const movies = [...this.state.movies];
+    const index = movies.indexOf(movie);
+    movies[index] = { ...movies[index] };
+    movies[index].liked = !movies[index].liked;
+    this.setState({ movies });
   };
 
   render() {
@@ -27,7 +37,8 @@ class Movies extends Component {
               <th>Genre</th>
               <th>Stock</th>
               <th>Rate</th>
-              <th></th>
+              <th />
+              <th />
             </tr>
           </thead>
           <tbody>
@@ -38,6 +49,12 @@ class Movies extends Component {
                 <td>{movie.genre.name}</td>
                 <td>{movie.numberInStock}</td>
                 <td>{movie.dailyRentalRate}</td>
+                <td>
+                  <Like
+                    liked={movie.liked}
+                    onClick={() => this.handleLike(movie)}
+                  />
+                </td>
                 <td>
                   <button
                     onClick={() => this.handleDelete(movie)} // arrow function to pass argument
